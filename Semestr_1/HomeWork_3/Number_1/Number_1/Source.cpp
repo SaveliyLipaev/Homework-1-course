4b9ea15ed6e1ba806ee533c4f3c708babc41b7a4
Savelii_Lipaev <sava.lipaev@mail.ru>
2018-10-06 15:22:11 +0300

    added void progeckt 3.1

---
+++ Semestr_1/HomeWork_3/Number_1/Number_1/Source.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void printmas(int* m, int length)
+{
+	for (int i = 0; i < length; i++)
+		printf("%d ", m[i]);
+	printf("\n");
+}
+void swap(int& a, int& b)
+{
+	const int c = a;
+	a = b;
+	b = c;
+}
+int partition(int* mas, int left, int right)
+{
+	int pivot = mas[right];
+	int i = left;
+	for (int j = left; j < right; j++)
+	{
+		if (mas[j] <= pivot)
+		{
+			swap(mas[i], mas[j]);
+			i++;
+		}
+	}
+	swap(mas[i], mas[right]);
+	printmas(mas, 7);
+	return i;
+}
+void qusort(int* mas, int left, int right)
+{
+	if (right < left)
+	{
+		return;
+	}
+	const int p = partition(mas, left, right);
+	qusort(mas, left, p - 1);
+	qusort(mas, p + 1, right);
+}
+
+
+int main()
+{
+	const int n = 7;
+	int array[n]{ 3, 5, 4, 9, 7, 2, 1 };
+	qusort(array, 0, 6);
+	printmas(array, 7);
+
+
+	int p;
+	scanf("%d", &p);
+	return 0;
+}
