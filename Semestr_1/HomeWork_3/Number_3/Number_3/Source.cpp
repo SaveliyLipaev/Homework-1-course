191622f722046b24a59a2c3c6e75413fad1474ee
Savelii_Lipaev <sava.lipaev@mail.ru>
2018-10-08 02:02:38 +0300

    Добавил функцию которая находит в отсартированном массиве наиболее часто встречаемый элемент

--- Semestr_1/HomeWork_3/Number_3/Number_3/Source.cpp
+++ Semestr_1/HomeWork_3/Number_3/Number_3/Source.cpp
@@ -4,20 +4,42 @@
 void swap(int &a, int &b);
 void siftingDown(int *mas, int root, const int border);
 void heapSort(int *mas, const int length);
+int mostCommonElement(int *mas, const int length);
 
 int main()
 {
-	int mas[8] = { 5,7,3,2,8,1,9,6 };
+	int mas[10] = { 1,1,2,3,3,3,8,8,8,8 };
 	
-	heapSort(mas, 8);
+	printf("%d\n",mostCommonElement(mas, 10));
 
-
-	for (int i = 0; i < 8; i++)
-		printf("%d ", mas[i]);
 
 	int n;
 	scanf("%d", &n);
 	return 0;
+}
+
+int mostCommonElement(int *mas, const int length)
+{
+	int amount = 1;
+	int maxAmount = 0;
+	int element = mas[0];
+	for (int i = 0; i < length - 1; ++i)
+	{
+		if (mas[i] == mas[i + 1])
+		{
+			amount++;
+		}
+		else
+		{
+			if (amount > maxAmount)
+			{
+				maxAmount = amount;
+				element = mas[i];
+			}
+			amount = 1;
+		}
+	}
+	return element;
 }
 
 void swap(int &a, int &b)
