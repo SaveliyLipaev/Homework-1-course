582f5c60c679aca4a4ffd7899259ddde7e9b1981
Savelii_Lipaev <sava.lipaev@mail.ru>
2018-11-06 01:56:00 +0300

    добавил комментарии

--- Semestr_1/HomeWork_6/Number_1/Number_1/FunctionFor6.1.h
+++ Semestr_1/HomeWork_6/Number_1/Number_1/FunctionFor6.1.h
@@ -5,7 +5,14 @@
 
 using namespace std;
 
+//The function returns true if the char is a character from 0..9
 bool isNumber(const char &symbol);
+
+//The function returns true if the char is a character "+,-,*,/"
 bool isOperator(const char &symbol);
+
+//the function returns the number that was obtained as a result of the operation
 int operation(const char &symbol, const int &operandOne, const int &operandTwo);
+
+//the function returns the value calculated by the arithmetic expression in the postfix form
 int expressionResult(const string &str);
