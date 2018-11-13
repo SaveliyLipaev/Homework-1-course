4f60a062a1fdeb18e4897bc9721ef96f5edc4f03
Savelii_Lipaev <sava.lipaev@mail.ru>
2018-11-14 02:07:08 +0300

    Поправил тест

--- Semestr_1/HomeWork_7/Number_1/Number_1/test.cpp
+++ Semestr_1/HomeWork_7/Number_1/Number_1/test.cpp
@@ -1,16 +1,5 @@
 #include "InteractiveWindow.h"
 
-struct BST
-{
-	NodeBST *head = nullptr;
-};
-
-struct NodeBST
-{
-	int data;
-	NodeBST *leftChild = nullptr;
-	NodeBST *rightChild = nullptr;
-};
 
 bool test()
 {
@@ -18,30 +7,23 @@ bool test()
 
 	add(tree, 50);
 	add(tree, 100);
-	if (tree->head->data != 50 || tree->head->rightChild->data != 100)
+	if (!find(tree, 50) || !find(tree, 100) || isEmpty(tree))
 	{
+		deleteBST(tree);
 		return false;
 	}
 	add(tree, 25);
-	add(tree, 35);
-	if (tree->head->leftChild->data != 25 || tree->head->leftChild->rightChild->data != 35)
+	terriblyUglyDeleteNode(tree, 50);
+	if (find(tree, 50))
 	{
-		return false;
-	}
-	if (!find(tree, 25) || !find(tree, 100) || find(tree, 10))
-	{
-		return false;
-	}
-	terriblyUglyDeleteNode(tree, 25);
-	if (tree->head->leftChild->data == 25 || tree->head->leftChild->data != 35)
-	{
+		deleteBST(tree);
 		return false;
 	}
 	terriblyUglyDeleteNode(tree, 100);
-	terriblyUglyDeleteNode(tree, 50);
-	terriblyUglyDeleteNode(tree, 35);
+	terriblyUglyDeleteNode(tree, 25);
 	if (!isEmpty(tree))
 	{
+		deleteBST(tree);
 		return false;
 	}
 	deleteBST(tree);
