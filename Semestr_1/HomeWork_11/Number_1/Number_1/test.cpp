4b34253ed22801bf12f4c9b649dc06ab3c01a742
SaveliyLipaev <sava.lipaev@mail.ru>
2018-12-13 17:02:22 +0300

    Новая домашка с нового для меня графического клиента гита

---
+++ Semestr_1/HomeWork_11/Number_1/Number_1/test.cpp
@@ -0,0 +1,19 @@
+#include "KMP.h"
+
+using namespace std;
+
+int readAndTest(string str)
+{
+	ifstream file("Text.txt");
+	int temp = KnutMorrisPrattForFile(file, str);
+	file.close();
+	return temp;
+}
+
+bool test()
+{
+	string str1 = "dda";
+	string str2 = "fff";
+	string str3 = "egj";
+	return readAndTest(str1) == -1 && readAndTest(str2) == 40 && readAndTest(str3) == 17;
+}
