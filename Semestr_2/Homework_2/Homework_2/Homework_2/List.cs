29241a1d9e83a3831fa9ce0e3bd0465eb8dd4df9
SaveliyLipaev <sava.lipaev@mail.ru>
2019-02-24 16:33:51 +0300

    Первая версия списка

---
+++ Semestr_2/Homework_2/Homework_2/Homework_2/List.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class List
+    {
+        private class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+        }
+
+        private int size;
+        private Node head;
+
+        private bool GoodPosition(int position)
+        {
+            return position > 0 && position <= size;
+        }
+
+        public bool AddPosition(int position, int data)
+        {
+            if (!GoodPosition(position))
+            {
+                return false;
+            }
+
+            if (position == 1)
+            {
+                Add(data);
+                return true;
+            }
+
+            var newNode = new Node(data, head);
+            ++size;
+
+            for (var i = 0; i < position - 2; ++i)
+            {
+                newNode.next = newNode.next.next;
+            }
+
+            var buffer = newNode.next.next;
+            newNode.next.next = newNode;
+            newNode.next = buffer;
+
+            return true;
+        }
+
+        public void Add(int data)
+        {
+            var newNode = new Node(data, head);
+            head = newNode;
+            ++size;
+        }
+
+        public int Pop()
+        {
+            if(this.IsEmpty())
+            {
+                Console.WriteLine("Ошибка, список пуст!!!");
+                return -1;
+            }
+
+            var value = head.data;
+            head = head.next;
+            --size;
+            return value;
+        }
+
+        public int PopPosition(int position)
+        {
+            if (!GoodPosition(position))
+            {
+                Console.WriteLine("Ошибка!!!!!");
+                return -1;
+            }
+
+            if (position == 1)
+            {
+                return this.Pop();
+            }
+
+            var buffer = head;
+
+            for (var i = 0; i < position - 2; ++i)
+            {
+                buffer = buffer.next;
+            }
+
+            var value = buffer.next.data;
+            buffer.next = buffer.next.next;
+            --size;
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public bool ChangeValue(int position, int value)
+        {
+            if (!GoodPosition(position))
+            {
+                Console.WriteLine("Ошибка!!!");
+                return false;
+            }
+
+            var buffer = head;
+
+            for (var i = 0; i < position - 1; ++i) 
+            {
+                buffer = buffer.next;
+            }
+
+            buffer.data = value;
+            return true;
+        }
+
+        public int GetValue(int position)
+        {
+            if (!GoodPosition(position)) 
+            {
+                Console.WriteLine("Ошибка!!!");
+                return -1;
+            }
+
+            var buffer = head;
+   
+            for (var i = 0; i < position - 1; ++i)
+            {
+                buffer = buffer.next;
+            }
+
+            return buffer.data;
+        }
+
+    }
+}
---
+++ Semestr_2/Homework_2/Homework_2/Homework_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List();
+            list.Add(4);
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+            list.AddPosition(3, 10);
+            list.ChangeValue(3, 99);
+
+            Console.WriteLine(list.GetValue(0));
+            Console.ReadKey();
+        }
+    }
+}
