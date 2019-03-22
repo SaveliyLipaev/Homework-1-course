5348ced8c7b76687a1c8e2b09e45782f37eab57c
SaveliyLipaev <sava.lipaev@mail.ru>
2019-03-22 09:38:45 +0300

    Добавление Списка

---
+++ Semestr_2/Homework_4/Homework_4.2/Homework_4.2/List.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+namespace Homework_4._2
+{
+    public class List : IList
+    {
+        protected Node head;
+        public int amountElements { get; private set; }
+        
+        protected class Node
+        {
+            public int data { get; set; }
+            public Node next { get; set; }
+
+            public Node(int data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+        }
+        
+        public virtual void Add(int data)
+        {
+            head = new Node(data, head);
+            ++amountElements;
+        }
+
+        public int Remove()
+        {
+            if(IsEmpty())
+            {
+                throw new NullReferenceException("Невозможно удалить элемент, список пуст");
+            }
+
+            int buffer = head.data;
+            head = head.next;
+            --amountElements;
+            return buffer;
+        }
+
+        public void RemoveElement(int data)
+        {
+            if (head != null && head.data == data)
+            {
+                Remove();
+                return;
+            }
+
+            var element = head;
+            var previousElement = head;
+
+            while (element != null)
+            {
+                if (element.data == data)
+                {
+                    previousElement.next = element.next;
+                    --amountElements;
+                    return;
+                }
+                previousElement = element; 
+                element = element.next;
+            }
+            throw new MissingItemException("Такого элемента не существует в списке");
+        }
+
+        public bool IsEmpty() => head == null;
+
+    }
+}
---
+++ Semestr_2/Homework_4/Homework_4.2/Homework_4.2/UniqueList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework_4._2
+{
+    public class UniqueList : List
+    {
+        private bool FindNode(int data)
+        {
+            var ptr = head;
+            while(ptr != null)
+            {
+                if (ptr.data == data) 
+                {
+                    return true;
+                }
+                ptr = ptr.next;
+            }
+            return false;
+        }
+
+        public override void Add(int data)
+        {
+            if (FindNode(data))
+            {
+                throw new AttemptAddAnExistingItemException("Невозможно добавить элемент в список, он уже существует");
+            }
+
+            base.Add(data);
+        }
+    }
+}
