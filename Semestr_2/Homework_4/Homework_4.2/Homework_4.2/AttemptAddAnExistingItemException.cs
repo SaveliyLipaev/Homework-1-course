c56fb49c6071861e6ff23477e6080abcfb796c4d
SaveliyLipaev <sava.lipaev@mail.ru>
2019-03-22 09:40:03 +0300

    Добавление исключений

---
+++ Semestr_2/Homework_4/Homework_4.2/Homework_4.2/AttemptAddAnExistingItemException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework_4._2
+{
+    public class AttemptAddAnExistingItemException : Exception
+    {
+        public AttemptAddAnExistingItemException()
+        { }
+
+        public AttemptAddAnExistingItemException(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ Semestr_2/Homework_4/Homework_4.2/Homework_4.2/MissingItemException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework_4._2
+{
+    public class MissingItemException : Exception
+    {
+        public MissingItemException()
+        { }
+
+        public MissingItemException(string message)
+            : base(message)
+        { }
+    }
+}
