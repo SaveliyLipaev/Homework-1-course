11975290e8c490209ed52ddca952cdc0e9957bbb
SaveliyLipaev <sava.lipaev@mail.ru>
2019-03-30 21:21:15 +0300

    Мэйник

---
+++ Semestr_2/Homework_4/HomeWork_4.1/HomeWork_4.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace HomeWork_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string expression;
+                using (var readFile = new StreamReader("FileForExpression.txt"))
+                {
+                    expression = readFile.ReadToEnd();
+                }
+
+                var tree = new ParseTree(expression);
+
+                Console.WriteLine(tree.GetExpression());
+                Console.WriteLine(tree.Result());
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
