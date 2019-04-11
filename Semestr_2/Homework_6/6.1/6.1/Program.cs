4c9640532fb1808725433c91e8ea83b4b67556fd
SaveliyLipaev <sava.lipaev@mail.ru>
2019-04-11 19:06:30 +0300

    Первый коммит, написал все функции, осталось тесты написать, yml - ник тоже изменил

---
+++ Semestr_2/Homework_6/6.1/6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(FunctionForTask.Fold(new List<int>() { 1, 2, 3 }, 1, (acc, elem) => acc * elem));
+
+        }
+    }
+}
---
+++ Semestr_2/Homework_6/6.1/6.1Tests/FunctionForTaskTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _6._1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1.Tests
+{
+    [TestClass()]
+    public class FunctionForTaskTests
+    {
+        [TestMethod()]
+        public void MapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FoldTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
