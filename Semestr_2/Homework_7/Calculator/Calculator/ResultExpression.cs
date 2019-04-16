d0dff0be5311867fa9727d3398566174b1e7f81c
SaveliyLipaev <sava.lipaev@mail.ru>
2019-04-16 19:51:01 +0300

    Вроде готов

---
+++ Semestr_2/Homework_7/Calculator/Calculator/ResultExpression.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Класс реализующий вычисление выражения
+    /// </summary>
+    public static class ResultExpression
+    {
+        public static double Result(string expression)
+        {
+            var postfix = InPostfixForm(ParseExpression(expression));
+            return CalculatePostfix(postfix);
+        }
+
+        /// <summary>
+        /// Функция парсит данную строку
+        /// </summary>
+        private static List<string> ParseExpression(string expression)
+        {
+            var list = new List<string>();
+            string buffer = string.Empty;
+            foreach (var symbol in expression)
+            {
+                if (symbol >= '0' && symbol <= '9' || symbol == ',')
+                {
+                    buffer += symbol;
+                }
+                else if (IsOperator(symbol.ToString()) || symbol.ToString() == ")" || symbol.ToString() == "(")
+                {
+                    if (buffer != string.Empty)
+                    {
+                        list.Add(buffer);
+                        buffer = string.Empty;
+                    }
+
+                    list.Add(symbol.ToString());
+                }
+            }
+            if (buffer != string.Empty)
+            {
+                list.Add(buffer);
+            }
+            return list;
+        }
+
+        private static bool IsOperator(string ch) => ch == "+" || ch == "*" || ch == "/" || ch == "-";
+
+        /// <summary>
+        /// Функция переводит строку в постфиксную форму
+        /// </summary>
+        private static string InPostfixForm(List<string> expression)
+        {
+            var stack = new Stack<string>();
+            expression.Insert(0, "(");
+            expression.Add(")");
+            string postfix = string.Empty;
+
+            for (var index = 0; index < expression.Count;) 
+            {
+                if (expression[index] == "(")
+                {
+                    stack.Push("(");
+                    ++index;
+                }
+                else if (expression[index] == "*" || expression[index] == "/")
+                {
+                    if (stack.Peek() == "*" || stack.Peek() == "/")
+                    {
+                        postfix += stack.Pop() + " ";
+                    }
+                    else
+                    {
+                        stack.Push(expression[index]);
+                        ++index;
+                    }
+                }
+                else if (expression[index] == "+" || expression[index] == "-")
+                {
+                    if (stack.Count ==0 || stack.Peek() == "(")
+                    {
+                        stack.Push(expression[index]);
+                        ++index;
+                    }
+                    else
+                    {
+                        postfix += stack.Pop() + " ";
+                    }
+                }
+                else if (expression[index] == ")")
+                {
+                    if (stack.Peek() == "(")
+                    {
+                        stack.Pop();
+                        ++index;
+                    }
+                    else
+                    {
+                        postfix += stack.Pop() + " ";
+                    }
+                }
+                else if (double.TryParse(expression[index], out double result)) 
+                {
+                    postfix += expression[index] + " ";
+                    ++index;
+                }
+            }
+
+            if (stack.Count != 0)
+            {
+                throw new ArgumentException("Не удалось перевести выражение в постфиксную форму");
+            }
+
+            return postfix;
+        }
+        
+        /// <summary>
+        /// Функция, вычисляющая выражение в постфиксной форме
+        /// </summary>
+        private static double CalculatePostfix(string expression)
+        {
+            var stack = new Stack<double>();
+            string[] literals = expression.Split(' ');
+            foreach (var literal in literals)
+            {
+                if (double.TryParse(literal, out double result))
+                {
+                    stack.Push(result);
+                }
+                else if (IsOperator(literal))
+                {
+                    double secondNumber = stack.Pop();
+                    double firstNumber = stack.Pop();
+                    stack.Push(DoOperation(firstNumber, secondNumber, literal));
+                }
+            }
+
+            var buffer = stack.Pop();
+            if (stack.Count != 0)
+            {
+                throw new ArgumentException("Невозможно вычислить, проверьте правильность записи");
+            }
+
+            return buffer;
+        }
+
+        private static double DoOperation(double firstNumber, double secondNumber, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return firstNumber + secondNumber;
+                case "-":
+                    return firstNumber - secondNumber;
+                case "*":
+                    return firstNumber * secondNumber;
+                case "/":
+                    if (secondNumber == 0) 
+                    {
+                        throw new DivideByZeroException();
+                    }
+                    return firstNumber / secondNumber;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Semestr_2/Homework_7/Calculator/CalculatorTests/ResultExpressionTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class ResultExpressionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void ResultExpressionBadCharactersTest()
+        {
+            ResultExpression.Result("3+3*s+1");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void ResultExpressionInvalidInputTest()
+        {
+            ResultExpression.Result("(3*)");
+        }
+
+        [TestMethod()]
+        public void ResultExpressionTest()
+        {
+            Assert.AreEqual(24, ResultExpression.Result("2*(10+2)"));
+        }
+
+        [TestMethod()]
+        public void NegativeNumbersTest()
+        {
+            Assert.AreEqual(-20, ResultExpression.Result("10-(6/2)*10"));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void DivisionByZeroTest()
+        {
+            Assert.AreEqual(-1, ResultExpression.Result("3/0"));
+        }
+    }
+}
