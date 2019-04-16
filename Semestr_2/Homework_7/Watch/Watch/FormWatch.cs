75e943bbef104acde4c79c004f6d070e0fd87dec
SaveliyLipaev <sava.lipaev@mail.ru>
2019-04-16 21:18:47 +0300

    Tik tak

---
+++ Semestr_2/Homework_7/Watch/Watch/FormWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Watch
+{
+    /// <summary>
+    /// WATCH
+    /// </summary>
+    public partial class FormWatch : Form
+    {
+        public FormWatch()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Каждый тик юзается этот метод
+        /// </summary>
+        private void TimerSecond_Tick(object sender, EventArgs e)
+        {
+            tablo.Text = DateTime.Now.ToString("T");
+        }
+
+        /// <summary>
+        /// При запуске формы, запуститься таймер
+        /// </summary>
+        private void FormWatch_Load(object sender, EventArgs e)
+        {
+            timerSecond.Start();
+        }
+    }
+}
---
+++ Semestr_2/Homework_7/Watch/Watch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Watch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Watch")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("17b7ec8a-ab40-446a-8e14-661d3a18fc7c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
