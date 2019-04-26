efb0c08a9d199630b1fc2ef9a5d679e704b8cda1
SaveliyLipaev <sava.lipaev@mail.ru>
2019-04-26 23:46:20 +0300

    Более менее готовая версия, нужны тесты и комментарии

---
+++ Semestr_2/Homework_8/Set/Set/MySet.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Set
+{
+    public class MySet<T> : ISet<T> where T : IComparable<T>
+    {
+        private Node head;
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        private class Node
+        {
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+            public T Item { get; set; }
+            public Node(T item)
+            {
+                Item = item;
+            }
+        }
+
+        public bool Add(T item)
+        {
+            if (head == null)
+            {
+                head = new Node(item);
+                ++Count;
+                return true;
+            }
+            else
+            {
+                return FindPositionAndAdd(head, item);
+            }
+        }
+
+        private bool FindPositionAndAdd(Node node, T item)
+        {
+            if (item.CompareTo(node.Item) == 0)
+            {
+                return false;
+            }
+            if (item.CompareTo(node.Item) > 0)
+            {
+                if (node.RightChild == null)
+                {
+                    node.RightChild = new Node(item);
+                    ++Count;
+                    return true;
+                }
+                return FindPositionAndAdd(node.RightChild, item);
+            }
+            else
+            {
+                if (node.LeftChild == null)
+                {
+                    node.LeftChild = new Node(item);
+                    ++Count;
+                    return true;
+                }
+                return FindPositionAndAdd(node.LeftChild, item);
+            }
+        }
+
+        public void Clear()
+        {
+            head = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var temp = head;
+            while (true)
+            {
+                if (temp == null)
+                {
+                    return false;
+                }
+                if (item.CompareTo(temp.Item) == 0)
+                {
+                    return true;
+                }
+                if (item.CompareTo(temp.Item) > 0)
+                {
+                    temp = temp.RightChild;
+                }
+                else
+                {
+                    temp = temp.LeftChild;
+                }
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (Count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException();
+            }
+
+            var queue = new Queue<Node>();
+            queue.Enqueue(head);
+
+            while (queue.Count > 0)
+            {
+                var temp = queue.Dequeue();
+                array[arrayIndex] = temp.Item;
+                ++arrayIndex;
+
+                if (temp.LeftChild != null)
+                {
+                    queue.Enqueue(temp.LeftChild);
+                }
+
+                if (temp.RightChild != null)
+                {
+                    queue.Enqueue(temp.RightChild);
+                }
+            }
+        }
+
+        private bool FindNode(Node node, T item)
+        {
+            if (item.CompareTo(node.Item) == 0)
+            {
+                return true;
+            }
+            if (item.CompareTo(node.Item) > 0)
+            {
+                if (node.RightChild == null)
+                {
+                    return false;
+                }
+                return FindNode(node.RightChild, item);
+            }
+            else
+            {
+                if (node.LeftChild == null)
+                {
+                    return false;
+                }
+                return FindNode(node.LeftChild, item);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            var countBefore = Count;
+
+            head = DoDeleteNode(head, item);
+
+            if (countBefore == Count)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private Node DoDeleteNode(Node node, T item)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            else if (item.CompareTo(node.Item) > 0)
+            {
+                node.RightChild = DoDeleteNode(node.RightChild, item);
+            }
+            else if (item.CompareTo(node.Item) < 0)
+            {
+                node.LeftChild = DoDeleteNode(node.LeftChild, item);
+            }
+            else
+            {
+                --Count;
+                if (node.RightChild == null)
+                {
+                    return node.LeftChild;
+                }
+                else
+                {
+                    var right = node.RightChild;
+                    var left = node.LeftChild;
+                    var min = node.RightChild;
+                    var parentMin = node;
+
+                    while (min.LeftChild != null)
+                    {
+                        parentMin = min;
+                        min = min.LeftChild;
+                    }
+
+                    if (node.RightChild == min)
+                    {
+                        min.LeftChild = left;
+                        return min;
+                    }
+                    else
+                    {
+                        parentMin.LeftChild = min.RightChild;
+                        min.LeftChild = left;
+                        min.RightChild = right;
+                        return min;
+                    }
+                }
+            }
+            return node;
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            foreach (var temp in other)
+            {
+                Remove(temp);
+            }
+        }
+
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            var newSet = new MySet<T>();
+            foreach (var element in other) 
+            {
+                if (FindNode(head, element))
+                {
+                    newSet.Add(element);
+                }
+            }
+            head = newSet.head;
+            Count = newSet.Count;
+            newSet.Clear();
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            var tempSet = new MySet<T>();
+            foreach (var element in other) 
+            {
+                tempSet.Add(element);
+            }
+
+            foreach (var element in this)
+            {
+                if (!tempSet.FindNode(tempSet.head, element))
+                {
+                    tempSet.Clear();
+                    return false;
+                }
+            }
+
+            if (tempSet.Count > Count)
+            {
+                tempSet.Clear();
+                return true;
+            }
+
+            tempSet.Clear();
+            return false;
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            var count = 0;
+            foreach (var element in other)
+            {
+                if (!FindNode(head, element))
+                {
+                    return false;
+                }
+                ++count;
+            }
+
+            if (count == Count)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            var tempSet = new MySet<T>();
+            foreach (var element in other)
+            {
+                tempSet.Add(element);
+            }
+
+            foreach (var element in this)
+            {
+                if (!tempSet.FindNode(tempSet.head, element))
+                {
+                    tempSet.Clear();
+                    return false;
+                }
+            }
+
+            tempSet.Clear();
+            return true;
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            foreach (var element in other)
+            {
+                if (!FindNode(head, element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            foreach (var element in other)
+            {
+                if (FindNode(head, element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            var countOther = 0;
+            foreach (var element in other)
+            {
+                if (!FindNode(head, element))
+                {
+                    return false;
+                }
+                ++countOther;
+            }
+
+            if (countOther == Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            foreach (var element in other)
+            {
+                if (FindNode(head, element))
+                {
+                    Remove(element);
+                }
+                else
+                {
+                    Add(element);
+                }
+            }
+        }
+
+        public void UnionWith(IEnumerable<T> other)
+        {
+            foreach (var element in other)
+            {
+                Add(element);
+            }
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            Add(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new SetEnumerator<T>(this);
+        }
+
+        private class SetEnumerator<T> : IEnumerator<T>
+        {
+            private T[] mas;
+            private int position = -1;
+
+            public SetEnumerator(ISet<T> set)
+            {
+                mas = new T[set.Count];
+                if (set.Count != 0)
+                {
+                    set.CopyTo(mas, 0);
+                }
+            }
+            public T Current
+            {
+                get
+                {
+                    if (position == -1 || position >= mas.Length)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return mas[position];
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            { }
+
+            public bool MoveNext()
+            {
+                if (position < mas.Length - 1)
+                {
+                    position++;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+    }
+}
---
+++ Semestr_2/Homework_8/Set/Set/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set = new MySet<int>();
+            var list = new List<int>() { 1, 5, 4, 10, 9 ,0};
+
+            set.Add(1);
+            set.Add(5);
+            set.Add(4);
+            set.Add(9);
+            set.Add(10);
+            //set.Add(12);
+            //set.Add(11);
+            set.UnionWith(list);
+            foreach (var el in set)
+            {
+                Console.WriteLine(el);
+            }
+
+
+        }
+    }
+}
