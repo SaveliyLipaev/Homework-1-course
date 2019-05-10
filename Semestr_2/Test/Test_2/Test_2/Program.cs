03349f2a6c7cd1d6f1a0c5d53bd91c1b338dd659
SaveliyLipaev <sava.lipaev@mail.ru>
2019-05-10 15:11:09 +0300

    Готова

---
+++ Semestr_2/Test/Test_2/Test_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var masString = new string[] { "1111 2222", "33", "1111 2222 3333 4444", "1111 2222 3333 4444", "1111 2222 33333" };
+
+            var set = ArrayInSet(masString);
+
+            set.PrintSet();
+        }
+
+        /// <summary>
+        /// Функция возвращающая SortedSet состоящий из входного массива строк
+        /// </summary>
+        public static SortedSet<string> ArrayInSet(string[] mas)
+        {
+            var set = new SortedSet<string>();
+
+            foreach (var item in mas)
+            {
+                var tempArray = item.Split(' ');
+                var list = new List<string>();
+
+                foreach (var tempItem in tempArray)
+                {
+                    list.Add(tempItem);
+                }
+
+                set.Add(list);
+            }
+
+            return set;
+        }
+    }
+}
